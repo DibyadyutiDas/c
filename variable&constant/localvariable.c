6a0734d3904bd802e3b07510bfa2ea30a16c6a2e
Dibyadyuti Das <dibyadyutidas0@gmail.com>
2023-03-05 10:00:00 +0530

    Add variable and constant examples

---
+++ variable&constant/localvariable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+    void main()
+    {
+        int lvariable;
+        printf("Enter intger data -");
+        scanf("%d",&lvariable);
+        printf("You enter data %d",lvariable);
+    }
